b19a76c58f5219a2f1863c500aa2f990d4ee1b7b
hugo dupuis <hugo.dupuis2@ecoles-epsi.net>
2025-04-17 19:01:14 +0200

    First commit

---
+++ src/main/java/com/seenrr/seenrr/SeenrrApplication.java
@@ -0,0 +1,13 @@
+package com.seenrr.seenrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeenrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeenrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seenrr/seenrr/SeenrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seenrr.seenrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeenrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
